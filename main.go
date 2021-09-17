30fc0dd4fceeb51533052d3ea4e75767b778b558
helong <helong@kingsoft.com>
2021-09-17 17:50:28 +0800

    NF 测试

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	str1 := "1|1|1|1|1|"
-	str2 := "1|1|1|1|1"
+	str2 := "1|1|1|1|1|1|"
 	arr1 := strings.Split(str1, "|")
 	arr2 := strings.Split(str2, "|")
 	fmt.Println(len(arr1))
