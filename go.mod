df6b480a39ae3babb7fa2bae4738a94aaaad9441
helong <helong@kingsoft.com>
2021-11-05 18:06:54 +0800

    NF 添加测试代码

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module golang-study
 
 go 1.16
+
+require github.com/ethereum/go-ethereum v1.10.11
--- main.go
+++ main.go
@@ -1,15 +1,41 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"strings"
+	"log"
+	"math"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func main() {
-	str1 := "1|1|1|1|1|"
-	str2 := "1|1|1|1|1|1|"
-	arr1 := strings.Split(str1, "|")
-	arr2 := strings.Split(str2, "|")
-	fmt.Println(len(arr1))
-	fmt.Println(len(arr2))
+	client, err := ethclient.Dial("https://mainnet.infura.io")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	account := common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balance) // 25893180161173005034
+
+	blockNumber := big.NewInt(5532993)
+	balanceAt, err := client.BalanceAt(context.Background(), account, blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balanceAt) // 25729324269165216042
+
+	fbalance := new(big.Float)
+	fbalance.SetString(balanceAt.String())
+	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+	fmt.Println(ethValue) // 25.729324269165216041
+
+	pendingBalance, err := client.PendingBalanceAt(context.Background(), account)
+	fmt.Println(pendingBalance) // 25729324269165216042
 }
